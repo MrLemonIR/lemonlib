9087051730d749e6d621c4953628fd35c00fd2ed
MrLemonYT <thelemonyt09@gmail.com>
2025-08-28 16:53:44 +0330

    Create __init__.py

---
+++ lemonlib/__init__.py
@@ -0,0 +1,7 @@
+# __init__.py
+__author__ = "MrLeMoNIR"
+__version__ = "0.1.0"
+
+# توابع و کلاس‌هایی که می‌خوای مستقیم در دسترس باشن
+from .core import fast_print, fast_connect
+from .utils import timer
